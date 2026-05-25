f16361c85acfde6ab26577f144e86d0747603535
jesagui28mx <jesagui28@gmail.com>
2026-05-25 14:44:25 -0700

    Update Dashboard.py

--- pages_admin/Dashboard.py
+++ pages_admin/Dashboard.py
@@ -1,1 +1,10 @@
+import streamlit as st
 
+st.subheader("📊 Dashboard")
+
+col1,col2,col3,col4=st.columns(4)
+
+col1.metric("Programados","8")
+col2.metric("Completados","5")
+col3.metric("Pendientes","2")
+col4.metric("Reprogramados","1")
