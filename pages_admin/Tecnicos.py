8b221760e3eb0de36ad8fc3304ae5c190c6a1ad9
jesagui28mx <jesagui28@gmail.com>
2026-05-25 14:45:09 -0700

    Update Tecnicos.py

--- pages_admin/Tecnicos.py
+++ pages_admin/Tecnicos.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import pandas as pd
 
+st.subheader("👨‍🔧 Técnicos")
+
+tecnicos = pd.DataFrame([
+    {"Nombre":"Carlos","Zona":"Norte","Estado":"Disponible"},
+    {"Nombre":"Miguel","Zona":"Centro","Estado":"Ocupado"}
+])
+
+st.dataframe(tecnicos, use_container_width=True)
