95046de5777876c43255f96cf845790986fdf868
jesagui28mx <jesagui28@gmail.com>
2026-05-25 14:17:23 -0700

    Update Agenda.py

--- pages/Agenda.py
+++ pages/Agenda.py
@@ -1,1 +1,16 @@
+import streamlit as st
 
+st.title("📅 Agenda")
+
+st.table([
+{
+"Hora":"09:00",
+"Técnico":"Carlos",
+"Cliente":"Juan Pérez"
+},
+{
+"Hora":"11:30",
+"Técnico":"Miguel",
+"Cliente":"Hotel Sol"
+}
+])
