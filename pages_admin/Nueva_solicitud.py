b78a22057b8acc3c2cc62bea324db88cf1709de5
jesagui28mx <jesagui28@gmail.com>
2026-05-25 14:45:39 -0700

    Update Nueva_solicitud.py

--- pages_admin/Nueva_solicitud.py
+++ pages_admin/Nueva_solicitud.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+st.subheader("📝 Nueva solicitud")
+
+st.text_input("Cliente")
+st.selectbox(
+    "Servicio",
+    ["Mantenimiento","Instalación","Reparación"]
+)
+
+st.button("Guardar")
