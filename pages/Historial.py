f750b3f6562768ab309fcf6ea9d9390ae622df85
jesagui28mx <jesagui28@gmail.com>
2026-05-25 14:17:55 -0700

    Update Historial.py

--- pages/Historial.py
+++ pages/Historial.py
@@ -1,1 +1,16 @@
+import streamlit as st
 
+st.title("📁 Historial")
+
+st.write("Juan Pérez")
+
+st.info("""
+Febrero 2026
+Mantenimiento
+
+Abril 2026
+Revisión fuga
+
+Próximo recomendado:
+Agosto 2026
+""")
